f92445f6c76925cdd16f60914b07f5a4a9f45db1
Janson <jansonchiu343@gmail.com>
2020-06-14 23:15:03 -0700

    add comments to tweet.js

--- learningReact/projectreact/src/Tweet.js
+++ learningReact/projectreact/src/Tweet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'; 
+
+// Props are paramenters that are passed into the component
 function Tweet(props) { 
   return (
     <div className="tweet"> 
       <h3>{props.name}</h3>
-      <p>This is a random Tweet</p>
+      <p>{props.message}</p>
       <h3>Number of likes</h3>
     </div> 
   );
